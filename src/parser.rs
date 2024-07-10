d8d7d8696c5edbd936aee687e96968364f9c7610
UtaUtaUtau <jmctayag@gmail.com>
2024-07-10 23:30:21 +0800

    switch to allow negative numbers

--- src/parser.rs
+++ src/parser.rs
@@ -11,21 +11,21 @@ pub struct ResamplerArgs {
     pub out_file: String,
     #[arg(value_parser = pitch_parser)]
     pub pitch: i32,
-    #[arg(allow_hyphen_values = true)]
+    #[arg(allow_negative_numbers = true)]
     pub velocity: f64,
     #[arg(default_value_t = String::from(""))]
     pub flags: String,
-    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
+    #[arg(allow_negative_numbers = true, default_value_t = 0.)]
     pub offset: f64,
-    #[arg(allow_hyphen_values = true, default_value_t = 1000.)]
+    #[arg(allow_negative_numbers = true, default_value_t = 1000.)]
     pub length: f64,
-    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
+    #[arg(allow_negative_numbers = true, default_value_t = 0.)]
     pub consonant: f64,
-    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
+    #[arg(allow_negative_numbers = true, default_value_t = 0.)]
     pub cutoff: f64,
-    #[arg(allow_hyphen_values = true, default_value_t = 100.)]
+    #[arg(allow_negative_numbers = true, default_value_t = 100.)]
     pub volume: f64,
-    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
+    #[arg(allow_negative_numbers = true, default_value_t = 0.)]
     pub modulation: f64,
     #[arg(value_parser = tempo_parser, default_value_t = 100.)]
     pub tempo: f64,
