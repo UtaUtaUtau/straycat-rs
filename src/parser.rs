fd27a1ed981828a745901077b924aa23a8ec61f3
UtaUtaUtau <jmctayag@gmail.com>
2024-07-10 20:26:23 +0800

    allow hyphens

--- src/parser.rs
+++ src/parser.rs
@@ -5,25 +5,27 @@ use clap::Parser;
 #[command(name = "straycat-rs")]
 #[command(version = "0.1.0")]
 #[command(about = "WORLD-based UTAU resampler on Rust.")]
+#[command()]
 pub struct ResamplerArgs {
     pub in_file: String,
     pub out_file: String,
     #[arg(value_parser = pitch_parser)]
     pub pitch: i32,
+    #[arg(allow_hyphen_values = true)]
     pub velocity: f64,
     #[arg(default_value_t = String::from(""))]
     pub flags: String,
-    #[arg(default_value_t = 0.)]
+    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
     pub offset: f64,
-    #[arg(default_value_t = 1000.)]
+    #[arg(allow_hyphen_values = true, default_value_t = 1000.)]
     pub length: f64,
-    #[arg(default_value_t = 0.)]
+    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
     pub consonant: f64,
-    #[arg(default_value_t = 0.)]
+    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
     pub cutoff: f64,
-    #[arg(default_value_t = 100.)]
+    #[arg(allow_hyphen_values = true, default_value_t = 100.)]
     pub volume: f64,
-    #[arg(default_value_t = 0.)]
+    #[arg(allow_hyphen_values = true, default_value_t = 0.)]
     pub modulation: f64,
     #[arg(value_parser = tempo_parser, default_value_t = 100.)]
     pub tempo: f64,
