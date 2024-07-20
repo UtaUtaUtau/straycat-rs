e6f758ad8f7dc52c7eb02ca41aef23f40fc68a46
UtaUtaUtau <29729824+UtaUtaUtau@users.noreply.github.com>
2024-07-20 20:46:27 +0800

    switch to iterator for linspace

--- src/util.rs
+++ src/util.rs
@@ -7,16 +7,16 @@ pub fn arange(end: i32) -> Vec<f64> {
 }
 
 pub fn linspace(start: f64, end: f64, num: usize, endpoint: bool) -> Vec<f64> {
-    let mut result = Vec::with_capacity(num);
-    for i in 0..num {
-        let ratio = if endpoint {
-            i as f64 / (num as f64 - 1.)
-        } else {
-            i as f64 / num as f64
-        };
-        result.push(start + (end - start) * ratio);
-    }
-    result
+    (0..num)
+        .map(|i| {
+            let ratio = if endpoint {
+                i as f64 / (num - 1) as f64
+            } else {
+                i as f64 / num as f64
+            };
+            start + (end - start) * ratio
+        })
+        .collect()
 }
 
 pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
